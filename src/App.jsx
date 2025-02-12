73275ec611b696b370e5ec86b384933c860f2103
Alexander Bouthillet <alex.bouthillet@gmail.com>
2025-02-12 12:04:46 -0500

    add navbar to page for easy acces to home

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { Routes, Route } from 'react-router-dom'
 
 import Homepage from "./components/Homepage.jsx"
 import SingleBookDetails from './components/BookDetails.jsx'
+import NavBar from './components/NavBar.jsx'
 
 const App = () => {
   
   return (
     <>
     <h1>Book Buddy</h1>
+
+    <NavBar />
 
     <Routes>
       <Route path='/' element={<Homepage />}/>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+const NavBar = () => {
+  return (
+    <nav id="nav">
+      <Link to='/'>Home Page</Link>
+    </nav>
+  )
+}
+
+export default NavBar;

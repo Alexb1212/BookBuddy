323fe41ab41a1e625e1f84add76b23298e69d50c
Alexander Bouthillet <alex.bouthillet@gmail.com>
2025-02-12 11:24:25 -0500

    add home page and display titles and images

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+
+import Homepage from "./components/Homepage.jsx"
+
 const App = () => {
   
   return (
     <>
     <h1>Book Buddy</h1>
+
+    <Routes>
+      <Route path='/' element={<Homepage />}/>
+      <Route path='/book-details/:id' element={<h2>Details of books</h2>}/>
+    </Routes>
     </>
   )
 }
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+
+const Homepage = () => {
+  const [ books, setBooks ] = useState([]);
+
+  useEffect(() => {
+
+    const getAllBooks = async() => {
+      const response = await fetch('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books')
+      console.log(response)
+      const jsonObj = await response.json();
+      console.log(jsonObj)
+      const allBooks = jsonObj.books
+      console.log(allBooks)
+
+      setBooks(allBooks);
+    }
+    getAllBooks();
+  }, [])
+
+  return (
+    <>
+      <h2>Home Page</h2>
+      {
+        books.map((singleBook) => {
+          return (
+            <Link key={singleBook.id} to={`/book-details/${singleBook.id}`}>
+              <h3>{singleBook.title}</h3>
+
+              <img src={singleBook.coverimage} height={150} width={100}/>
+
+            </Link>
+          )
+        })
+      }
+    </>
+  )
+}
+
+export default Homepage;
